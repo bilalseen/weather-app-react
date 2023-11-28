3e12b074a3111d495044974131547d131f31d6f6
bilalseen <bilalsen2010@gmail.com>
2023-11-28 18:14:50 +0300

    Used async/await in getWeatherData

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -1,36 +1,40 @@
 import React from "react";
 import { useState } from "react";
-import { API } from "../../API.js";
+import API from "../../API.js";
 import "./WeatherApp.css";
 import { SearchBar } from "../SearchBar/SearchBar.jsx";
 import { WeatherImage } from "../WeatherImage/WeatherImage.jsx";
 import { WeatherInfo } from "../WeatherInfo/WeatherInfo.jsx";
 
 export const WeatherApp = (props) => {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState(null);
   const [city, setCity] = useState("City/Town");
-  const [temp, setTemp] = useState("");
-  const [wind, setWind] = useState("");
-  const [humidity, setHumidity] = useState("");
-  const [weather, setWeather] = useState("");
+  const [temp, setTemp] = useState(null);
+  const [wind, setWind] = useState(null);
+  const [humidity, setHumidity] = useState(null);
+  const [weather, setWeather] = useState(null);
 
-  const getWeatherData = () => {
-    if (search != "") {
-      fetch(`${API.base}weather?q=${search}&units=Metric&APPID=${API.key}`)
-        .then((response) => response.json())
-        .then((data) => {
-          setCity(search);
-          setTemp(Math.floor(data.main.temp));
-          setWind(Math.floor(data.wind.speed));
-          setHumidity(Math.floor(data.main.humidity));
-          setWeather(data.weather[0].icon);
-          setSearch("");
-        })
-        .catch((err) => {
-          alert("We didn't find the city. Please enter again");
-          setCity("City/Town");
-        });
-    } else alert("Please enter city/town");
+  const getWeatherData = async () => {
+    try {
+      if (search !== "") {
+        const response = await fetch(
+          `${API.base}weather?q=${search}&units=Metric&APPID=${API.key}`
+        );
+        const data = await response.json();
+
+        setCity(search);
+        setTemp(Math.floor(data.main.temp));
+        setWind(Math.floor(data.wind.speed));
+        setHumidity(Math.floor(data.main.humidity));
+        setWeather(data.weather[0].icon);
+        setSearch("");
+      } else {
+        alert("Please enter city/town");
+      }
+    } catch (err) {
+      alert("We didn't find the city. Please enter again");
+      setCity("City/Town");
+    }
   };
   return (
     <div className="weatherapp-container">
