0f02b31412d905ef4d86e52eb830af57e352e1a9
bilalseen <bilalsen2010@gmail.com>
2023-11-28 18:17:18 +0300

    useState hook sets have been changed

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,22 +2,22 @@ import React, { useState } from "react";
 import "./SearchBar.css";
 import search_icon from "../../assets/icons/search.png";
 
-export const SearchBar = (props) => {
+export const SearchBar = ({ onChange, onClick, search }) => {
   return (
     <div className="searchbar-container">
       <input
         type="text"
         className="search-input"
         placeholder="Search City/Town"
-        value={props.search}
-        onChange={props.onChange}
+        value={search || ""}
+        onChange={onChange}
       ></input>
 
       <img
         src={search_icon}
         className="search-icon"
         alt="search icon"
-        onClick={props.onClick}
+        onClick={onClick}
       />
     </div>
   );
